f32ab8ebd509df7e815fb96189974e7db44af3e3
SunDwarf <sun@veriny.tf>
2017-01-01 17:04:44 +0000

    Add load and unload commands.

--- plugins/owner.py
+++ plugins/owner.py
@@ -6,10 +6,16 @@ from curious.commands.context import Context
 from curious.commands.plugin import Plugin
 
 
+def is_owner(self, ctx: Context):
+    return ctx.author.id == 141545699442425856 or ctx.message.author.id == ctx.bot.application_info.owner.id
+
+
 class Owner(Plugin):
     """
     Owner-only commands.
     """
+    plugin_check = is_owner
+
     @commands.command(name="eval")
     async def _eval(self, ctx: Context, *, eval_str: str):
         msg = await ctx.channel.send("Evaluating...")
@@ -25,3 +31,19 @@ class Owner(Plugin):
 
         fmtted = "```py\n{}\n```".format(result)
         await msg.edit(fmtted)
+
+    @commands.command(name="load", invokation_checks=[is_owner])
+    async def _load(self, ctx: Context, *, import_name: str):
+        """
+        Loads a plugin.
+        """
+        await self.bot.load_plugins_from(import_name)
+        await ctx.message.channel.send(":heavy_check_mark: Loaded.")
+
+    @commands.command(name="unload", invokation_checks=[is_owner])
+    async def _unload(self, ctx: Context, *, import_name: str):
+        """
+        Unloads a plugin.
+        """
+        await self.bot.unload_plugins_from(import_name)
+        await ctx.message.channel.send(":heavy_check_mark: Unloaded.")
