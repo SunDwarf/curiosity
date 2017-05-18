a1290d105329d40a19c6430fd1f60b58713b2b70
Laura <l@veriny.tf>
2017-05-18 16:56:38 +0100

    Remove some old, unused code
    Signed-off-by: Laura <l@veriny.tf>

--- curiosity/plugins/fuyu.py
+++ curiosity/plugins/fuyu.py
@@ -1,8 +1,3 @@
-import random
-
-import curio
-import romkan
-
 from curious.commands import command
 from curious.commands.context import Context
 from curious.commands.plugin import Plugin
@@ -56,82 +51,16 @@ class Fuyu(Plugin):
                                        params={
                                            "client_id": str(bot_id),
                                            "scope": "bot"
-                                           },
+                                       },
                                        headers=headers,
                                        json=payload)
 
             js = await response.json()
             if response.status_code != 200:
-                await ctx.channel.send("\N{NO ENTRY SIGN} Failed to add bot to server! Error `{}`".format(js))
+                await ctx.channel.send("\N{NO ENTRY SIGN} Failed to add bot to server! "
+                                       "Error `{}`".format(js))
             else:
                 if 'location' in js and 'invalid_request' in js['location']:
                     await ctx.channel.send("\N{NO ENTRY SIGN} Invalid client ID.")
                 else:
                     await ctx.channel.send("\N{THUMBS UP SIGN} Added new bot.")
-
-    @command(aliases=["shouting"], invokation_checks=[has_admin])
-    async def screaming(self, ctx: Context):
-        """
-        Makes the server screaming.
-        """
-        coros = []
-        for member in ctx.guild.members.values():
-            coros.append(await curio.spawn(member.change_nickname(member.user.name.upper())))
-
-        async with ctx.channel.typing:
-            results = await curio.gather(coros, return_exceptions=True)
-        exc = sum(1 for x in results if isinstance(x, Exception))
-
-        await ctx.channel.send("AAAAAAAAAAAAAAAA (`{}` changed, `{}` failed)".format(len(results) - exc, exc))
-
-    @command(aliases=["librarian"], invokation_checks=[has_admin])
-    async def whispering(self, ctx: Context):
-        """
-        Makes the server quiet.
-        """
-        coros = []
-        for member in ctx.guild.members.values():
-            coros.append(await curio.spawn(member.change_nickname(member.user.name.lower())))
-
-        async with ctx.channel.typing:
-            results = await curio.gather(coros, return_exceptions=True)
-
-        exc = sum(1 for x in results if isinstance(x, Exception))
-
-        await ctx.channel.send(":zzz: (`{}` changed, `{}` failed)".format(len(results) - exc, exc))
-
-    @command(invokation_checks=[has_admin])
-    async def massnick(self, ctx: Context, prefix: str = "", suffix: str = ""):
-        """
-        Mass-nicks the server.
-        """
-        coros = []
-        for member in ctx.guild.members.values():
-            coros.append(await curio.spawn(member.change_nickname("{}{}{}".format(prefix, member.user.name, suffix))))
-
-        async with ctx.channel.typing:
-            results = await curio.gather(coros, return_exceptions=True)
-
-        exc = sum(1 for x in results if isinstance(x, Exception))
-
-        await ctx.channel.send(":100: (`{}` changed, `{}` failed)".format(len(results) - exc, exc))
-
-    @command(invokation_checks=[has_admin])
-    async def weebify(self, ctx: Context):
-        """
-        Weebifys the server.
-        """
-        coros = []
-        for member in ctx.guild.members.values():
-            r = random.choice([1, 2])
-            if r == 1:
-                name = romkan.to_katakana(member.name)
-            elif r == 2:
-                name = romkan.to_hiragana(member.name)
-            coros.append(await curio.spawn(member.change_nickname(name)))
-
-        async with ctx.channel.typing:
-            results = await curio.gather(coros, return_exceptions=True)
-
-        exc = sum(1 for x in results if isinstance(x, Exception))
-        await ctx.channel.send("Kawaii sugoi desu! (`{}` changed, `{}` failed)".format(len(results) - exc, exc))
